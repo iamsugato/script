7aaa0db16d16c1a5b9536c3a7496305691702807
iamsugato <97702758+iamsugato@users.noreply.github.com>
2022-01-14 02:12:53 +0530

    Add files via upload

---
+++ Vcenter VAMI backup status.ps1
@@ -0,0 +1,27 @@
+﻿Write-Host "Welcome to script to get status of latest VAMI backup"
+
+Write-Host "                        "
+
+
+$Server = Read-Host -Prompt 'Input Vcenter IP or FQDN'
+$User = Read-Host -Prompt 'Input the username has access to VCSA'
+$Pass = Read-Host -Prompt 'Input password'
+$Date = Get-Date
+
+Write-Host "                        "
+
+Write-Host "Please wait while we try to connect.... "
+
+connect-cisserver -Server $Server -User $USer -Password $Pass
+
+$backupjob = Get-CisService -Name com.vmware.appliance.recovery.backup.job 
+
+$result = $backupjob.list() 
+
+Write-Host "                        "
+
+Write-Host "Status of latest VAMI backup for '$Server'"
+
+Write-Host "                        "
+
+$backupjob.get($result[0]) | select-object id, start_time, end_time, progress, state  
